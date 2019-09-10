d25cba207093d1529d06f152ddc4ed08751ba766
Ayush Roy <ayushroy.official.2005@gmail.com>
2019-09-10 21:46:27 +0530

    Reset Email Feature Corrected

--- src/flaskblog/routes.py
+++ src/flaskblog/routes.py
@@ -152,13 +152,14 @@ def user_posts(username):
     return render_template('user_posts.html', posts=posts, user=user)
 
 def send_reset_email(user):
-    token = User.get_reset_token()
-    msg = Message('Password Reset Request', sender='noreply@gmail.com', recipients=[user.email])
+    token = User.get_reset_token(user)
+    msg = Message('Password Reset Request', sender='blablabla@gmail.com', recipients=[user.email])
     msg.body = f'''To reset your password, visit following link:
 {url_for('reset_token', token=token, _external=True)}
 
 If you did not make this request then simply ignore this email and no changes will hapen
 '''
+    mail.send(msg)
 
 @app.route("/reset_password", methods=['GET', 'POST'])
 def reset_request():
@@ -185,7 +186,7 @@ def reset_token(token):
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
-        flash('Your password has been updated! You are now able to log in', 'success')
+        flash('Your password has been updated! You are now able to log in.', 'success')
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
 
