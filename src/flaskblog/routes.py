8cbd69d8eb17dec15a173f7012576d16c7c22a0d
Ayush Roy <ayushroy.official.2005@gmail.com>
2019-09-10 11:39:10 +0530

    Add pagination feature

--- src/flaskblog/routes.py
+++ src/flaskblog/routes.py
@@ -8,9 +8,9 @@ from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
-@app.route("/index")
 def index():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=4, page=page)
     return render_template('index.html', posts=posts, title='Home')
 
 
@@ -139,6 +139,15 @@ def delete_post(post_id):
         db.session.commit()
         flash('Your post has been deleted succesfully!', 'success')
         return redirect(url_for('index'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=4)
+    return render_template('user_posts.html', posts=posts, user=user)
 
 
 
